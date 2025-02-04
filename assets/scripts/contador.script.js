318bf36481e59844d35538c85e8ffca864fb9f11
jessicaprestelo1201 <jessica.p.jesus@aluno.senai.br>
2025-02-04 13:38:39 -0300

    Feat: Create contador

---
+++ assets/scripts/contador.script.js
@@ -0,0 +1,19 @@
+function dim() {
+    let atual = Number(document.getElementById("contador").innerHTML);
+        let result = atual -1;
+    
+    document.getElementById("contador").innerHTML = result }
+
+    function aum() {
+        let atual = Number(document.getElementById("contador").innerHTML);
+            let result = atual +1;
+        
+        document.getElementById("contador").innerHTML = result }
+
+        function res() {
+            let atual = Number(document.getElementById("contador").innerHTML);
+                let result = atual = 0;
+            
+            document.getElementById("contador").innerHTML = result }
+
+
